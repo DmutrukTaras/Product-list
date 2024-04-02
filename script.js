a5a5e7a9167e42b0c9e85309d2c1f4eec117deb2
Taras <dmutruk.taras@gmail.com>
2024-04-02 15:34:41 +0300

    add reset btn

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ const form = document.getElementById('task-form'); // Форма для дода
 const input = document.getElementById('task-input'); // Поле для введення завдання
 const taskList = document.getElementById('task-list'); // Список завда
 
-function addTask(taskText) {
+function addTask(taskText, taskCompleted = false) {
     const li = document.createElement('li');
     li.className = 'task-item';
     li.textContent = taskText;
+    if (taskCompleted) {
+        li.classList.add('completed');
+    }
     taskList.appendChild(li);
     li.addEventListener('click', toggleTask);
     
@@ -16,7 +19,7 @@ function addTask(taskText) {
 function saveTasksToLocalStorage() {
     const tasks = [];
     document.querySelectorAll('.task-item').forEach(task => {
-        tasks.push(task.textContent);
+        tasks.push([task.textContent, task.classList.contains('completed')]);
     });
     localStorage.setItem('tasks', JSON.stringify(tasks));
 }
@@ -35,12 +38,17 @@ form.addEventListener('submit', function(e) {
     }
 });
 
+form.addEventListener('reset', function(e) {
+    e.preventDefault(); // Зупиняємо стандартну поведінку форми
+    localStorage.clear();
+    taskList.innerHTML='';
+});
 
 window.addEventListener('load', function() {
     const tasks = JSON.parse(localStorage.getItem('tasks'));
     if (tasks) {
         tasks.forEach(task => {
-            addTask(task);
+            addTask(...task);
         });
     }
 });
